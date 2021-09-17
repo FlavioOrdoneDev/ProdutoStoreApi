16c8ad8c26a9cd9770c34a7f3326b168ad5ede19
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2021-09-17 17:16:11 -0300

    Adicionado ProdutoValidador e adicionado os métodos na Entidade Produto

--- ProdutoStoreApi.Dominio/Entidades/Produto.cs
+++ ProdutoStoreApi.Dominio/Entidades/Produto.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using ProdutoStoreApi.Dominio.Validacoes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ProdutoStoreApi.Dominio.Entidades
@@ -26,5 +29,26 @@ namespace ProdutoStoreApi.Dominio.Entidades
         public bool Perecivel { get; private set; }
         public int CategoriaId { get; private set; }
         public Categoria Categoria { get; private set; }
+
+        public void SetarAtivo(bool valor)
+        {
+            Ativo = valor;
+        }
+        
+        public void SetarPerecivel(bool valor)
+        {
+            Perecivel = valor;
+        }
+
+        public bool IsValid()
+        {
+            ProdutoValidador produtoConsistente = new ProdutoValidador();
+            ResultadoValidacao = produtoConsistente.Validate(this);
+
+            return ResultadoValidacao.IsValid;
+        }
+
+        [NotMapped]
+        public ValidationResult ResultadoValidacao { get; set; }
     }
 }
---
+++ ProdutoStoreApi.Dominio/Validacoes/ProdutoValidador.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using ProdutoStoreApi.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdutoStoreApi.Dominio.Validacoes
+{
+    public class ProdutoValidador : AbstractValidator<Produto>
+    {
+        public ProdutoValidador()
+        {
+            RuleFor(produto => produto.Nome).NotNull().WithMessage("O nome é obrigatório.");
+            RuleFor(produto => produto.Nome).MinimumLength(3).WithMessage("O nome não pode ter menos de 3 caracteres.");
+            RuleFor(produto => produto.Nome).MaximumLength(100).WithMessage("O nome não pode ter mais que 100 caracteres.");
+
+            RuleFor(produto => produto.Descricao).NotNull().WithMessage("A descrição é obrigatória.");
+            RuleFor(produto => produto.Descricao).MinimumLength(3).WithMessage("A descrição não pode ter menos de 3 caracteres.");
+            RuleFor(produto => produto.Descricao).MaximumLength(250).WithMessage("A descrição não pode ter mais que 250 caracteres.");
+
+            RuleFor(produto => produto.CategoriaId).NotNull().WithMessage("A categoria é obrigatória");
+        }
+    }
+}
