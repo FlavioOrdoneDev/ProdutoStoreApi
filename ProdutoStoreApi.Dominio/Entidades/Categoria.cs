90b7dc2d969782cf4add3bf06e381a0eb6398a24
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2021-09-17 14:25:27 -0300

    Foram adicionadas as entidades Produto e Categoria no Dominio

---
+++ ProdutoStoreApi.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdutoStoreApi.Dominio.Entidades
+{
+    public class Categoria
+    {
+        public Categoria(){}
+        public Categoria(string nome, string descricao, bool ativo = true){
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+    }
+}
---
+++ ProdutoStoreApi.Dominio/Entidades/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProdutoStoreApi.Dominio.Entidades
+{
+    public class Produto
+    {
+
+        public Produto(){}
+
+        public Produto(string nome, string descricao, bool ativo, bool perecivel, int categoriaId)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Perecivel = perecivel;
+            CategoriaId = categoriaId;
+        }
+
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public bool Perecivel { get; private set; }
+        public int CategoriaId { get; private set; }
+        public Categoria Categoria { get; private set; }
+    }
+}
